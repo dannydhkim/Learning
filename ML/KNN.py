96eefc41ed84b58818cc364aa51d0445aa8f5259
Donghyeok Kim <kr.dannydhkim@gmail.com>
2021-09-18 00:13:22 -0700

    Finish implementing KNN algorithm

--- ML/KNN.py
+++ ML/KNN.py
@@ -1,17 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import mode
 
-class k_nearest_neighbors:
+class k_nearest_neighbors():
     def __init__(self, k):
         self.k = k
     
-    def calculateDistance(x1, y1, x2, y2):
-        return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    def calculateDistance(self,x1, y1, x2, y2):
+        return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     
     
-    def nearestNeighbors():
-
-
+    def nearestNeighbors(self, data, target):
+        distances = {}
+        for i in range(data.shape[1]):
+            distances[self.calculateDistance(target[0],target[1],data[0][i],data[1][i])] = (i, data[2][i])
+        self.neighbors = []
+        votes = []
+        for key in sorted(distances.keys())[:self.k]:
+            self.neighbors.append((distances[key][0], key))
+            votes.append(distances[key][1])
+        most_voted = mode(votes)
+        return self.neighbors, most_voted[0][0]
 
 def main():
     X = np.random.randint(20, size=50)
@@ -25,6 +34,11 @@ def main():
             category.append(1)
         else:
             category.append(2)
+    
+    target_X = input("X")
+    target_y = input("y")
+    target = [float(target_X),float(target_y)]
+    data = np.array((X,y,category))
 
     cdict= {0:'red',1:'blue', 2:'green'}
     fig, ax = plt.subplots()
@@ -34,5 +48,13 @@ def main():
     ax.legend()
     plt.show()
 
+    model = k_nearest_neighbors(3)
+    _, target_label = model.nearestNeighbors(data, target)
+    print(target, 'categorized as:', target_label)
+
+    ax.scatter(target[0], target[1], target_label)
+    plt.show()
+
 if __name__ == "__main__" : 
     main()
+
